160ca2a4e75c0a180034795a55be640a035d28ad
Alaa <alaaalrma@gmail.com>
2023-11-17 13:11:25 +0200

    TRIAL

---
+++ MathApp/React/src/multiEditor.tsx
@@ -0,0 +1,14 @@
+﻿// multiEditor.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './Editor';
+
+const editors = ['reactEditor1', 'reactEditor2', 'reactEditor3'];
+
+editors.forEach(editorId => {
+    const editorContainer = document.getElementById(editorId);
+    if (editorContainer) {
+        ReactDOM.render(<Editor />, editorContainer); 
+    }
+});
